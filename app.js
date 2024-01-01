ebd57440fe235821649c9af4c77a01dc681fc1d0
Natnael Teklemariam <natnaelmverse1@gmail.com>
2024-01-01 15:46:33 +0300

    cleared some comments

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ require('dotenv').config()
 const app = express();
 
 //middlewares
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
-// app.use(cors())
 
 // database and server config
 require('./config/dbConfig')()
